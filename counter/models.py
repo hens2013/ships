899f8d42b40b642beed5ae729974c29e04c0b480
hens2013 <hens2013@gmail.com>
2022-07-31 13:09:13 +0300

    update

--- counter/models.py
+++ counter/models.py
@@ -7,6 +7,6 @@ class Point(models.Model):
     x = models.FloatField()
     y = models.FloatField()
 
-    #calculate distance beetween two points accroding to the formula
+    #calculate distance between two points according to the formula
     def calculate_distance(self, point):
         return sqrt(pow(self.x - point.x, 2) + pow(self.y - point.y, 2))
